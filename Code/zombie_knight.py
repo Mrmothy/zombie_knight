324ddfdaff89300eb791797821204e67222fbd26
Timothy Riddle <tim.riddle.tr@gmail.com>
2024-10-15 10:16:08 -0600

    Added the class blue prints

--- Code/zombie_knight.py
+++ Code/zombie_knight.py
@@ -14,6 +14,192 @@ pygame.display.set_caption("Zombie Knight")
 FPS = 60
 clock = pygame.time.Clock()
 
+#TODO Define classes
+class Game():
+    """A class to manage gameplay"""
+
+    def __init__(self):
+        """Initialize the game"""
+        pass
+
+    def update(self):
+        """Update the game"""
+        pass
+
+    def draw(self):
+        """Draw the game HUD"""
+        pass
+
+    def add_zombie(self):
+        """Add a zombie to the game"""
+        pass
+
+    def check_collisions(self):
+        """Check collisions that affect gameplay"""
+        pass
+
+    def check_round_completion(self):
+        """Check if the player survived a single night"""
+        pass
+
+    def check_game_over(self):
+        """Check to see if the player lost the game"""
+        pass
+
+    def start_new_round(self):
+        """Start a new night"""
+        pass
+
+    def pause_game(self):
+        """Pause the game"""
+        pass
+
+    def rest_game(self):
+        """Rest the game"""
+        pass
+
+class Tile(pygame.sprite.Sprite):
+    """A class to represent a 32x32 pixel area in our display"""
+
+    def __init__(self):
+        """Initialize the tile"""
+        pass
+
+class Player(pygame.sprite.Sprite):
+    """A class the user can control"""
+
+    def __init__(self):
+        """Initialize the player"""
+        pass
+    
+    def update(self):
+        """Update the player"""
+        pass
+
+    def move(self):
+        """Move the player"""
+        pass
+
+    def check_collisions(self):
+        """Check for collisions with platforms and portals"""
+        pass
+
+    def check_animations(self):
+        """Check to see if jump or fire animations should run"""
+        pass
+
+    def jump(self):
+        """Jump upwards if on a platform"""
+        pass
+
+    def fire(self):
+        """Fire a (bullet)"""
+        pass
+
+    def reset(self):
+        """Reset the player's position"""
+        pass
+
+    def animate(self):
+        """Animate the player's actions"""
+        pass
+
+class Bullet(pygame.sprite.Sprite):
+    """A projectile launched by the player"""
+
+    def __init__(self):
+        """Initialize the bullet"""
+        pass
+
+    def update(self):
+        """Update the bullet"""
+        pass
+
+class Zombie(pygame.sprite.Sprite):
+    """An enemy class to move across the screen"""
+
+    def __init__(self):
+        """Initialize the zombie"""
+        pass
+    
+    def update(self):
+        """Update the zombie"""
+        pass
+
+    def move(self):
+        """Move the zombie"""
+        pass
+
+    def check_collisions(self):
+        """Check for collisions with platforms and portals"""
+        pass
+
+    def check_animations(self):
+        """Check to see if death or rise animations should run"""
+        pass
+
+    def reset(self):
+        """Reset the zombie's position"""
+        pass
+
+    def animate(self):
+        """Animate the zombie's actions"""
+        pass
+
+class RubyMaker(pygame.sprite.Sprite):
+    """A tile that is animater. A Ruby will be generated here"""
+
+    def __init__(self):
+        """Initialize the ruby maker"""
+        pass
+
+    def update(self):
+        """Update the ruby maker"""
+        pass
+
+    def animate(self):
+        """Animate the ruby maker"""
+        pass
+
+class Ruby(pygame.sprite.Sprite):
+    """A class the player must collect to earn points and health"""
+
+    def __init__(self):
+        """Initialize a ruby"""
+        pass
+
+    def update(self):
+        """Update the ruby"""
+        pass
+
+    def move(self):
+        """Move the ruby"""
+        pass
+
+    def check_collisions(self):
+        """Check collisions with platforms and portals"""
+        pass
+
+    def animate(self):
+        """Animate the ruby"""
+        pass
+
+class Portal(pygame.sprite.Sprite):
+    """A class that if collided with will transport you"""
+
+    def __init__(self):
+        """Initialize the portal"""
+        pass
+
+    def update(self):
+        """Update the portal"""
+        pass
+
+    def animate(self):
+        """Animate the portal"""
+        pass
+
+
 #Load in a background image (must resize image)
 background_image = pygame.transform.scale(pygame.image.load(join('Assets', 'images', 'background.png')),(1280, 736))
 background_rect = background_image.get_rect()
